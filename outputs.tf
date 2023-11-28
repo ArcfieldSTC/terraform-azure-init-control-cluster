b1a935c6ccbcc33be837f6060f745daf0f428fb5
Eric Deitrick <eric.deitrick@arcfield.com>
2023-11-28 12:36:07 -0500

    fix: add aks name to outputs

--- outputs.tf
+++ outputs.tf
@@ -131,3 +131,7 @@ output "aks_id" {
   description = "The ID of the AKS Cluster"
   value       = azurerm_kubernetes_cluster.this.id
 }
+output "aks_name" {
+  description = "The name of the AKS Cluster"
+  value       = azurerm_kubernetes_cluster.this.name
+}
